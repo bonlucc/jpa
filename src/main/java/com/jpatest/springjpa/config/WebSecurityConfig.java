5cee2f22a6c15851727fc71f5cea88dba0af04f1
luizguilherme.santos <gggufs@academico.ufs.br>
2023-06-12 08:20:22 -0300

    b

--- src/main/java/com/jpatest/springjpa/config/WebSecurityConfig.java
+++ src/main/java/com/jpatest/springjpa/config/WebSecurityConfig.java
@@ -1,15 +1,33 @@
 package com.jpatest.springjpa.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+
+    private final String[] WHITE_LISTED_URLS = {
+        "/register"
+    };
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(11);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .antMatchers(WHITE_LISTED_URLS).permitAll();
+        return http.build();
     }
 }
